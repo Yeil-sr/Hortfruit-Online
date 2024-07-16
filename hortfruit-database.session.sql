f2d4088501587871d6a3b13250a403bf3b0bfcc1
Yeil-sr <Blackfootout@gmail.com>
2024-07-16 18:12:59 -0300

    refactor file

--- hortfruit-database.session.sql
+++ hortfruit-database.session.sql
@@ -1,1 +1,8 @@
-SELECT * FROM produto WHERE id = 3;
+CREATE TABLE pedido_item (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pedido_id INT NOT NULL,
+    produto_id INT NOT NULL,
+    quantidade INT NOT NULL,
+    valor_produto DECIMAL(10, 2) NOT NULL,
+    user_id INT NOT NULL
+);
